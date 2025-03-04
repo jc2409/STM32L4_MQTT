c12b04c1df10fd1f621feb1efd13542a7b53b5b8
Andrew Choi <andrew092331@gmail.com>
2025-03-04 13:05:27 +0000

    Delete Projects/B-L475E-IOT01A/Applications/WiFi/MQTT_Client/MDK-ARM/B-L475E-IOT01 directory

--- Projects/B-L475E-IOT01A/Applications/WiFi/MQTT_Client/MDK-ARM/B-L475E-IOT01/hts221.d
+++
@@ -1,5 +0,0 @@
-b-l475e-iot01\hts221.o: ..\..\..\..\..\..\Drivers\BSP\Components\hts221\hts221.c
-b-l475e-iot01\hts221.o: ..\..\..\..\..\..\Drivers\BSP\Components\hts221\hts221.h
-b-l475e-iot01\hts221.o: ..\..\..\..\..\..\Drivers\BSP\Components\hts221\../Common/hsensor.h
-b-l475e-iot01\hts221.o: C:\Users\jaecho01\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
-b-l475e-iot01\hts221.o: ..\..\..\..\..\..\Drivers\BSP\Components\hts221\../Common/tsensor.h
--- Projects/B-L475E-IOT01A/Applications/WiFi/MQTT_Client/MDK-ARM/B-L475E-IOT01/startup_stm32l475xx.d
+++
@@ -1,1 +0,0 @@
-b-l475e-iot01\startup_stm32l475xx.o: startup_stm32l475xx.s
